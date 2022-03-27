5a40157cb1537236dc318bd2540243044c0df9d8
Tomas Berezny <tberezny@inno.innovatrics.net>
2022-03-27 16:08:13 +0200

    added connection to db

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(
+    'mongodb+srv://BerkoAndDanci:BerkoAndDanciBestPeople@cluster0.b3qn0.mongodb.net/test',
+    () => console.log('connected to db')
+    )
 
 
 //routes
